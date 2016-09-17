8e567e34d8583433df455299620dbbe80bbdb11b
Alex Parkinson <okalex@gmail.com>
2016-09-17 11:32:27 -0700

    User ListView

--- mobile/components/UsersList.js
+++ mobile/components/UsersList.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native';
+import { ListView, Text, View } from 'react-native';
 import { graphql } from 'react-apollo'
 
 import api from '../api/client'
 
 class UsersList extends Component {
+  constructor(props) {
+    super(props)
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    this.state = {
+      dataSource: ds
+    }
+  }
+
+  componentWillReceiveProps(props) {
+    console.log(props)
+    const users = props.data.users || []
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(users)
+    })
+  }
+
+  renderUser(user) {
+    return <Text>{ user.id }: { user.name }</Text>
+  }
+
   render() {
     return (
-      <Text>{ JSON.stringify(this.props, null, 2) }</Text>
+      <View>
+        <ListView
+          dataSource={ this.state.dataSource }
+          renderRow={ this.renderUser } />
+      </View>
     )
   }
 }
